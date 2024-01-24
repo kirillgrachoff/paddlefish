6ade548c882b2179f702f177fe3b2d76866a5ad0
Kirill Grachev <kirill.grachoff@gmail.com>
2024-01-25 00:54:09 +0300

    Add test

---
+++ test/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <paddlefish/future.hpp>
+
+paddlefish::Future<int> Calculate(int value) {
+  std::cout << "Calculating " << value;
+  co_return value;
+}
+
+paddlefish::Future<> Main() {
+  auto f = Calculate(20);
+  std::cout << "co_await... ";
+  int v = co_await f;
+  int vv = co_await Calculate(40);
+  std::cout << "= " << v << std::endl;
+  std::cout << "Resumed successfully" << std::endl;
+  co_return {};
+}
+
+int main() {
+  auto f = Main();
+  std::cout << "Run" << std::endl;
+  f.run();
+  std::cout << "End" << std::endl;
+}
