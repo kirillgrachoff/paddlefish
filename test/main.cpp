351b9021443326418c265b004a90c3dec28c7a87
Kirill Grachev <kirill.grachoff@gmail.com>
2024-01-26 18:24:48 +0300

    Combine all tests in one

--- test/main.cpp
+++ test/main.cpp
@@ -25,29 +25,6 @@ paddlefish::Future<> recursive(int n = 0) {
   co_return {};
 }
 
-paddlefish::Future<> sequence() {
-  auto f = calculate(20);
-  std::cout << "co_await... ";
-  int v = co_await f;
-  std::cout << " = " << v << std::endl;
-  std::cout << "co_await... ";
-  int vv = co_await calculate(40);
-  std::cout << " = " << vv << std::endl;
-  auto vvv = co_await allocate(50);
-  std::cout << *vvv << std::endl;
-  std::cout << "Resumed successfully" << std::endl;
-  std::cout << "Recursive start" << std::endl;
-  co_await recursive();
-  std::cout << "Recursive end" << std::endl;
-  co_return {};
-}
-
-// paddlefish::Future<> parallel() {
-//   auto a = calculate(10);
-//   auto b = calculate(20);
-//   auto [a_r, b_r] = paddlefish::when_all(a, b);
-// }
-
 paddlefish::Future<> loop() {
   for (size_t i = 0; i < 1000; ++i) {
     if (i % 100 == 0) {
@@ -74,8 +51,37 @@ paddlefish::Future<> noexceptional() {
   co_return {};
 }
 
+paddlefish::Future<> sequence() {
+  auto f = calculate(20);
+  std::cout << "co_await... ";
+  int v = co_await f;
+  std::cout << " = " << v << std::endl;
+  std::cout << "co_await... ";
+  int vv = co_await calculate(40);
+  std::cout << " = " << vv << std::endl;
+  auto vvv = co_await allocate(50);
+  std::cout << *vvv << std::endl;
+  std::cout << "Resumed successfully" << std::endl;
+  std::cout << "Recursive start" << std::endl;
+  co_await recursive();
+  std::cout << "Recursive end" << std::endl;
+  std::cout << "Loop start" << std::endl;
+  co_await loop();
+  std::cout << "Loop end" << std::endl;
+  std::cout << "Exceptional start" << std::endl;
+  co_await noexceptional();
+  std::cout << "Exceptional end" << std::endl;
+  co_return {};
+}
+
+// paddlefish::Future<> parallel() {
+//   auto a = calculate(10);
+//   auto b = calculate(20);
+//   auto [a_r, b_r] = paddlefish::when_all(a, b);
+// }
+
 int main() {
   std::cout << "Run" << std::endl;
-  paddlefish::runtime::block_on(noexceptional());
+  paddlefish::runtime::block_on(sequence());
   std::cout << "End" << std::endl;
 }
